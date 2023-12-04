e505c71a89947f450c5ef550e2c34ab3fae5f31e
amir <amirkamalian24@gmail.com>
2023-12-04 20:16:10 +0330

    creating response in response class

--- web-server/response.cpp
+++ web-server/response.cpp
@@ -1,3 +1,90 @@
 #include "response.h"
 
 
+bool response::open_file(std::string PATH)
+{
+    file.open(PATH, std::ios::binary);
+    return file.is_open();
+}
+
+int response::get_file_size()
+{
+    return file.tellg();
+}
+
+
+std::string response::getContentType()
+{
+
+    if (received_request.path.find(".html") != std::string::npos) {
+        return "text/html";
+    }
+    else if (received_request.path.find(".css") != std::string::npos) {
+        return "text/css";
+    }
+    else if (received_request.path.find(".js") != std::string::npos) {
+        return "text/javascript";
+    }    
+    else if (received_request.path.find(".xml") != std::string::npos) {
+        return "text/xml";
+    }
+    else if (received_request.path.find(".png") != std::string::npos) {
+        return "image/png";
+    }
+    else if (received_request.path.find(".jpg") != std::string::npos) {
+        return "image/jpg";
+    }
+    else if (received_request.path.find(".jpeg") != std::string::npos) {
+        return "image/jpeg";
+    }    
+    else if (received_request.path.find(".woff2") != std::string::npos) {
+        return "font/woff2";
+    }
+    return "text/html";
+}
+
+std::string response::toString() const
+{
+    {
+        std::ostringstream request_stream;
+
+        // Start the request line
+        request_stream << "HTTP/" << received_request.http_version << " " << respone_code;
+
+        if (respone_code == 200) {
+            request_stream << " OK\r\n";
+        }
+        else if(respone_code == 404) {
+            request_stream << " Not Found\r\n";
+
+        }        
+        else if(respone_code == 301) {
+            request_stream << " Moved Permanently\r\n";
+
+        }
+
+        // Add headers
+        for (const auto& header : headers) {
+            request_stream << header.first << ": " << header.second << "\r\n";
+        }
+
+        request_stream << "\r\n"; // End of headers
+
+        // Add body if it is not empty
+        if (body.size()) {
+            request_stream.write(&body[0], body.size());
+        }
+
+        //std::cout << body.data() <<std::endl;
+
+        // Convert the string stream into a string
+        return request_stream.str();
+    }
+}
+
+
+void response::make_404() {
+
+    open_file("./html/404.html");
+    
+}
--- web-server/response.h
+++ web-server/response.h
@@ -1,14 +1,77 @@
 #pragma once
 #include <iostream>
 #include "request.h"
+#include <vector>
+#include <map>
+#include <iostream>
+#include <streambuf>
+#include <string>
+#include <fstream>
 
 
 class response
 {
 public:
 	response(std::string request) 
-		:received_request(request) {};
+		:received_request(request) {
+
+
+
+		//rols
+		if (false) {}
+
+		if (open_file("./html/" + received_request.path)) {
+
+			respone_code = 200;
+			headers["Content-Type"] = getContentType();
+			headers["Connection"] = "keep-alive";
+
+			body = std::vector<char>((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+			headers["Content-Length"] = std::to_string(body.size());
+
+
+			//std::cout << 132 <<std::endl;
+			//std::cout << headers["Content-Length"] <<std::endl;
+			//std::cout << headers["Content-Type"] <<std::endl;
+			//std::cout << body[0] <<std::endl;
+
+
+		}
+		else {
+
+			std::cout << received_request.path;
+
+			//open_file("./html/404.html");
+			//respone_code = 301;
+			respone_code = 404;
+			//headers["Location"] = "404.html";
+
+			headers["Connection"] = "close";
+			//body = std::vector<char>((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+			//std::cout << 700 << std::endl;
+			//std::cout << headers["Content-Length"] << std::endl;
+			//std::cout << headers["Content-Type"] << std::endl;
+			//std::cout << body[0] << std::endl;
+		}
+	
+	};
 
 	request received_request;
+
+	int respone_code;
+	std::map<std::string, std::string> headers;
+	std::vector<char> body;
+
+
+	std::ifstream file;
+
+	bool open_file(std::string PATH);
+	int get_file_size();
+
+	std::string getContentType();
+	std::string toString() const;
+	void make_404();
 };
 
